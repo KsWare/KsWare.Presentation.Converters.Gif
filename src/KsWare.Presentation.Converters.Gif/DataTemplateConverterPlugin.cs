6bdf1aae2220dd5e0cc049820342ebef2ace77ee
SchreinerK <ks@ksware.de>
2019-09-13 19:09:24 +0200

    Install-Package KsWare.XamlAnimatedGif.Wpf.StrongName -Version 1.2.2

--- src/KsWare.Presentation.Converters.Gif/DataTemplateConverterPlugin.cs
+++ src/KsWare.Presentation.Converters.Gif/DataTemplateConverterPlugin.cs
@@ -3,9 +3,8 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
-using System.Windows.Media.Imaging;
 using System.Xml;
-using WpfAnimatedGif;
+using XamlAnimatedGif;
 
 namespace KsWare.Presentation.Converters.Gif
 {
@@ -13,13 +12,14 @@ namespace KsWare.Presentation.Converters.Gif
 	{
 		public DataTemplate CreateDataTemplate(Uri locationUri)
 		{
-			// REQUIRES: PM> Install-Package WpfAnimatedGif
+			// REQUIRES: PM> Install-Package XamlAnimatedGif
+			// REQUIRES: PM> Install-Package KsWare.XamlAnimatedGif.Wpf.StrongName.1.2.2 (temporary)
 
-			ImageBehavior.SetAnimatedSource(new Image(), new BitmapImage()); //WORKAROUND
+			AnimationBehavior.SetSourceUri(new Image(), new Uri("pack://application,,,")); //WORKAROUND
 
 			var dataTemplateXaml = $@"<?xml version=""1.0"" encoding=""utf-8""?>
-<DataTemplate xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" xmlns:gif=""http://wpfanimatedgif.codeplex.com"">
-	<Image gif:ImageBehavior.AnimatedSource=""{locationUri}"" Stretch=""Uniform"" />
+<DataTemplate xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" xmlns:gif=""https://github.com/XamlAnimatedGif/XamlAnimatedGif"">
+	<Image gif:AnimationBehavior.SourceUri=""{locationUri}"" Stretch=""Uniform"" />
 </DataTemplate>";
 
 			var sr = new StringReader(dataTemplateXaml);
