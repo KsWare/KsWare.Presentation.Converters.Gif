4cbeb80093794fa7fcb31035c9d447174a93a5c6
SchreinerK <ks@ksware.de>
2020-01-28 11:51:53 +0100

    returns an Image, if Target Type not Control-/DataTemple

--- src/KsWare.Presentation.Converters.Gif/ResourceConverterPlugin.cs
+++ src/KsWare.Presentation.Converters.Gif/ResourceConverterPlugin.cs
@@ -20,11 +20,26 @@ namespace KsWare.Presentation.Converters.Gif {
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
 			if (typeof(DataTemplate).IsAssignableFrom(targetType)) return CreateDataTemplate(value);
 			if (typeof(ControlTemplate).IsAssignableFrom(targetType)) return CreateControlTemplate(value);
+			return CreateImage(value);
 			throw new NotImplementedException("Conversion not supported.");
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
 			throw new NotImplementedException();
+		}
+
+		private DataTemplate CreateImage(object content) {
+			var locationUri = (Uri)content;
+			AnimationBehavior.SetSourceUri(new Image(), new Uri("pack://application,,,")); //WORKAROUND
+
+			var templateXaml = $@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Image xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" xmlns:gif=""https://github.com/XamlAnimatedGif/XamlAnimatedGif"" 
+       gif:AnimationBehavior.SourceUri=""{locationUri}"" Stretch=""Uniform"" />
+";
+
+			var sr = new StringReader(templateXaml);
+			var xr = XmlReader.Create(sr);
+			return (DataTemplate)XamlReader.Load(xr);
 		}
 
 		private DataTemplate CreateDataTemplate(object content) {
